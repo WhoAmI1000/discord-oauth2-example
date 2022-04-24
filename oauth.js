6cf49aa6e43ad384cfc47784c2bd8c665bdbfad2
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2022-04-24 14:47:32 -0700

    Import node-fetch asynchronously; edited redirect_uri

--- oauth.js
+++ oauth.js
@@ -1,10 +1,10 @@
 /* Import dependencies */
 const Express = require('express'); // Import express
-const { URLSearchParams } = require('url'); // import URLSearchParams from url. You can also use form-data (in that case, this line would be const FormData = require('form-data');). If you do, be sure to adjust your variable names.
+const { URLSearchParams } = require('url'); // import URLSearchParams from url. You can also use form-data (const FormData = require('form-data');).
 const axios = require('axios'); // Import Axios
 const path = require('path'); // Import path
 const bodyParser = require('body-parser'); // Import body-parser
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args)); // Import node-fetch asynchronously; see https://www.npmjs.com/package/node-fetch#installation for more info on why this is done.
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args)); // Import node-fetch asynchronously; see https://www.npmjs.com/package/node-fetch#installation for more info on why this is done.
 
 /* Client Variables */
 const client_id = ''; // Paste your bot's ID here
@@ -12,7 +12,7 @@ const client_secret = ''; // Paste your bot's secret here
 
 /* Define app variables */
 const app = Express(); // Create a web app
-const port = 7100; // Port to host on, can be anything. HTTP is by default port 80. If you want to use https (so the URL would be https://localhost/), then the port would be 443.
+const port = 80; // Port to host on
 
 /* Make a function to give us configuration for the Discord API */
 function make_config(authorization_token) { // Define the function
@@ -42,9 +42,9 @@ app.post('/user', (req, res) => { // Will run when there are any incoming POST r
     data_1.append('client_id', client_id); // Append the client_id variable to the data
     data_1.append('client_secret', client_secret); // Append the client_secret variable to the data
     data_1.append('grant_type', 'authorization_code'); // This field will tell the Discord API what you are wanting in your initial request.
-    data_1.append('redirect_uri', `http://localhost:${port}/`); // This is the redirect URL where the user will be redirected when they finish the Discord login
+    data_1.append('redirect_uri', `http://localhost:${port}`); // This is the redirect URL where the user will be redirected when they finish the Discord login
     data_1.append('scope', 'identify'); // This tells the Discord API what info you would like to retrieve. You can change this to include guilds, connections, email, etc.
-    data_1.append('code', req.body); // This is a key parameter in our upcoming request. It is the code the user got from logging in. This will help us retrieve a token which we can use to get the user's info.
+    data_1.append('code', req.body) // This is a key parameter in our upcoming request. It is the code the user got from logging in. This will help us retrieve a token which we can use to get the user's info.
 
     fetch('https://discord.com/api/oauth2/token', { method: "POST", body: data_1 }).then(response => response.json()).then(data => { // Make a request to the Discord API with the form data, convert the response to JSON, then take it and run the following code.
         axios.get("https://discord.com/api/users/@me", make_config(data.access_token)).then(response => { // Make a request yet again to the Discord API with the token from previously.
@@ -55,6 +55,6 @@ app.post('/user', (req, res) => { // Will run when there are any incoming POST r
         });
     });
 });
-app.listen(port, function() { // Configure the app to "listen" on the specified port.
+app.listen(port, function () { // Configure the app to "listen" on the specified port.
     console.log(`App listening! Link: http://localhost:${port}/`); // Log the fact our app is ready to the console.
 });
